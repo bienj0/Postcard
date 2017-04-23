a95b45884eaf44f50fd76d0dcd241af8477d86cb
Robin Slot <vantslot@MacBook-van-Robin.local>
2017-04-23 10:56:29 +0200

    done

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,13 @@ class ViewController: UIViewController {
         messageLabel.isHidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.red
+        
+        nameLabel.isHidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blue
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
